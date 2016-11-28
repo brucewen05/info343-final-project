f71bce1b70735576d1268c72152fbcf2e4803f4e
Qingda Wen <qw9@uw.edu>
2016-11-28 10:39:09 -0800

    Add config for firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+var config = {
+    apiKey: "AIzaSyC5kCRuH4inxJadApuiGxSon7E8b0zdLsM",
+    authDomain: "info-343-final-project.firebaseapp.com",
+    databaseURL: "https://info-343-final-project.firebaseio.com",
+    storageBucket: "info-343-final-project.appspot.com",
+    messagingSenderId: "907757567327"
+  };
+  firebase.initializeApp(config);
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
