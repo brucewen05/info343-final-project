16b1c61a6ba40e7c9336a7b203af2e9e1e0692d8
Amy Wang <amyewang115@gmail.com>
2016-11-30 09:51:38 -0800

    change header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
-import {Layout, Drawer, Navigation, Content, Icon} from 'react-mdl';
 import './App.css';
 import firebase from 'firebase';
+import AppBar from 'material-ui/AppBar';
+import DrawerMenu from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui/svg-icons/navigation/menu';
+import ArrowIcon from 'material-ui/svg-icons/navigation/arrow-back';
+import {Layout, Drawer, Navigation, Content, Icon} from 'react-mdl';
+
+
+
+
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {open: false};
     this.handleSignOut = this.handleSignOut.bind(this);
   }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
 
   handleSignOut() {
     if (firebase.auth().currentUser) {
@@ -20,23 +40,32 @@ class App extends Component {
 
   render() {
     return (
-      <Layout fixedDrawer={true}>
-        <Drawer>
-            <Navigation>
-                <Link to="/main">Main Page</Link>
-                <Link to="/discussions">Discussion board</Link>
-                <Link to="/FAQs">FAQs</Link>
-                <Link to="/events">Events</Link>
-                {/*need to look at how to deal with this later! */}
-                <a onClick={this.handleSignOut} className="sign-out-button"><Icon name="input" />{' '}Sign out</a>
-            </Navigation>
-        </Drawer>
-        <Content>
+      <div>
+      <MuiThemeProvider>
+        <AppBar
+          title="Title"
+          iconElementLeft={<IconButton><MenuIcon /></IconButton>}
+          onLeftIconButtonTouchTap={this.handleToggle}
+        >
+        <DrawerMenu
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem onTouchTap={this.handleClose} href="/#/main">Homepage</MenuItem>
+          <MenuItem onTouchTap={this.handleClose} href="/#/discussions">Discussions</MenuItem>
+          <MenuItem onTouchTap={this.handleClose} href="/#/FAQs">FAQ</MenuItem>
+          <MenuItem onTouchTap={this.handleClose} href="/#/events">Events</MenuItem>
+          <MenuItem onTouchTap={this.handleSignOut}><IconButton><ArrowIcon/></IconButton>Sign out</MenuItem>
+        </DrawerMenu>
+      </AppBar>
+      </MuiThemeProvider>
+      <Content>
           {this.props.children}
         </Content>
-    </Layout>
+    </div>
     );
   }
 }
-
 export default App;
