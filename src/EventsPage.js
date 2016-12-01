5d02d8ea07498b46d41316938a9a31120fca0efc
Alyssa Holzer <holzealy@uw.edu>
2016-12-01 11:02:37 -0800

    add firebase to events page and display live data

--- src/EventsPage.js
+++ src/EventsPage.js
@@ -1,93 +1,141 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { Grid, Cell, Header, Button } from 'react-mdl';
+import firebase from 'firebase';
+import moment from 'moment';
 
 
 class EventsPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-
-        this.handleTitleChange = this.handleTitleChange.bind(this);
-        this.handleDateChange = this.handleDateChange.bind(this);
-        this.handleTimeChange = this.handleTimeChange.bind(this);
-        this.handleLocationChange = this.handleLocationChange.bind(this);
-        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
-    }
-
-    //handles title input
-    handleTitleChange(event) {
-        this.setState({title: event.target.value});
-    }
-
-    //handles date input
-    handleDateChange(event) {
-        this.setState({date: event.target.value});
-    }
-
-    //handles time input
-    handleTimeChange(event) {
-        this.setState({time: event.target.value});
-    }
-
-    //handles location input
-    handleLocationChange(event) {
-        this.setState({location: event.target.value});
-    }
-
-    //handles description input
-    handleDescriptionChange(event) {
-        this.setState({description: event.target.value});
-    }
-
-
     render() {
-        var result = [];
-        for (var i = 20; i < 30; i++) {
-            result.push(<EventsItem eventId={i} key={i} />);
-        }
         return (
             <div>
                 <Header title="Events" />
                 <Grid>
-                    <div className="list-group">
-                        <Cell col={12} className="list-group-item">
-                            <h2>Add a new event</h2>
-                            <form>
-                                <input type="text" className="form-control" onChange={this.handleTitleChange} placeholder="Title" /> <br />
-                                <input type="text" className="form-control" onChange={this.handleDateChange} placeholder="Date" /> <br />
-                                <input type="text" className="form-control" onChange={this.handleTimeChange} placeholder="Time" /> <br />
-                                <input type="text" className="form-control" onChange={this.handleLocationChange} placeholder="Location" /> <br />
-                                <textarea type="text" className="form-control" onChange={this.handleDescriptionChange} placeholder="Event description" /> <br />
-                            </form>
-                            <Button type="button" colored raised>Post Event</Button>
-                        </Cell>
-                        {result}
-                    </div>
+
+                    <NewEvent />
+                    <EventsList />
+
                 </Grid>
             </div>
         );
     }
 }
 
-class EventsItem extends React.Component {
+class NewEvent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { title: "", date: "", time: "", location: "", description: "" };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    //handles title input
+    handleInputChange(event, field) {
+        this.setState({ [field]: event.target.value });
+    }
+
+    handlePostEvent(event) {
+        event.preventDefault();
+        var user = firebase.auth().currentUser;
+        var eventsRef = firebase.database().ref("events");
+        //data about the event to store
+        var eventData = {
+            userId: user.uid,
+            displayName: user.displayName,
+            title: this.state.title,
+            date: this.state.date,
+            time: this.state.time,
+            location: this.state.location,
+            description: this.state.description,
+            postTime: firebase.database.ServerValue.TIMESTAMP
+        }
+        this.setState({
+            title: "",
+            date: "",
+            time: "",
+            location: "",
+            description: ""
+        });
+        eventsRef.push(eventData);
+    }
+
     render() {
         return (
-            <Cell col={12}>
-                <Link to={"/event/" + this.props.eventId} className="list-group-item" >
-                    <h2 className="list-group-item-heading">Event Title {this.props.eventId} </h2>
-                    <p>Date: MM/DD/YY</p>
-                    <p>Time: HH:MM</p>
-                    <p>Location: Lorem ipsum</p>
-                    <hr />
-                    <p className="list-group-item-text">
-                        Et sint sunt consequat labore officia. Aliquip consectetur cillum non qui do adipisicing labore consectetur. Commodo sit id commodo consectetur mollit reprehenderit amet mollit qui sint. Dolor aute eu consequat ex ad amet excepteur nostrud occaecat id culpa elit dolore. Labore mollit do esse sit veniam adipisicing elit cupidatat dolor.
-                </p>
-                    <br />
-                    <p>Event created MM/DD/YY by XXXXXXX</p>
-
-                </Link>
+            <Cell col={12} className="list-group-item">
+                <h2>Add a new event</h2>
+                <form>
+                    <input type="text" className="form-control" value={this.state.title} onChange={(event) => this.handleInputChange(event, "title")} placeholder="Title" /> <br />
+                    <input type="text" className="form-control" value={this.state.date} onChange={(event) => this.handleInputChange(event, "date")} placeholder="Date" /> <br />
+                    <input type="text" className="form-control" value={this.state.time} onChange={(event) => this.handleInputChange(event, "time")} placeholder="Time" /> <br />
+                    <input type="text" className="form-control" value={this.state.location} onChange={(event) => this.handleInputChange(event, "location")} placeholder="Location" /> <br />
+                    <textarea type="text" className="form-control" value={this.state.description} onChange={(event) => this.handleInputChange(event, "description")} placeholder="Event description" /> <br />
+                </form>
+                <Button type="button" onClick={(event) => this.handlePostEvent(event)} aria-label="post event" colored raised>Post Event</Button>
             </Cell>
+        );
+    }
+}
+
+class EventsList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { events: [] };
+    }
+
+    componentWillMount() {
+        var eventsRef = firebase.database().ref('events').orderByChild('time');
+        eventsRef.on('value', (snapshot) => {
+            var eventsArray = [];
+            snapshot.forEach(function (child) {
+                var event = child.val();
+                event.key = child.key;
+                eventsArray.push(event);
+            });
+            //updates list
+            this.setState({ events: eventsArray });
+        });
+        //listens for changes to user obj in database and stores in state
+        var usersRef = firebase.database().ref('users');
+        usersRef.on('value', (snapshot) => {
+            this.setState({ users: snapshot.val() });
+        });
+    }
+
+    componentWillUnmount() {
+        //unregister user and message listeners
+        firebase.database().ref('users').off();
+        firebase.database.ref('events').off
+    }
+
+    render() {
+        var eventItems = this.state.events.map((event) => {
+            return <EventItem event={event} eventId={event.key} key={event.key} />
+        })
+
+        return (
+            <Cell col={12}>
+                {eventItems}
+            </Cell>
+        );
+    }
+}
+
+
+class EventItem extends React.Component {
+    render() {
+        return (
+            <Link to={"/event/" + this.props.eventId} className="list-group-item">
+                <h2 className="list-group-item-heading">{this.props.event.title}</h2>
+                <p>Date: {this.props.event.date}</p>
+                <p>Time: {this.props.event.time}</p>
+                <p>Location: {this.props.event.location}</p>
+                <hr />
+                <p className="list-group-item-text">
+                    {this.props.event.description}
+                </p>
+                <br />
+                <p>Event created {moment(this.props.event.postTime).fromNow()} by {this.props.event.displayName}</p>
+            </Link>
         );
     }
 }
