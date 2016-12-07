f824777e9c4f758fc370d08275e0fc82dd8c5f59
Alyssa Holzer <holzealy@uw.edu>
2016-12-06 17:32:03 -0800

    remove grid from event details page and add styling

--- src/EventDetailsPage.js
+++ src/EventDetailsPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, hashHistory } from 'react-router';
-import { Grid, Cell } from 'react-mdl';
 import firebase from 'firebase';
 import moment from 'moment';
 
@@ -49,25 +48,27 @@ class EventDetailsPage extends React.Component {
 
     render() {
         return (
-            <Grid>
-                <Cell col={12}>
-                    <h1>{this.state.title}</h1>
-                </Cell>
-                <Cell col={12} className="list-group-item">
-                    <p>Date: {this.state.date}</p>
-                    <p>Time: {this.state.time}</p>
-                    <p>Location: {this.state.location}</p>
-                    <hr />
-                    <p className="list-group-item-text">
-                        {this.state.description}
-                    </p>
-                    <br />
-                    <p>Event created {moment(this.state.postTime).fromNow()}by {this.state.displayName}</p>
-                </Cell>
-                <Cell>
-                    <Link to={"/events"}>Back to all events</Link>
-                </Cell>
-            </Grid>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>{this.state.title}</h1>
+                        <div className="list-group-item">
+                            <p><span className="event-info">Date</span>: {this.state.date}</p>
+                            <p><span className="event-info">Time</span>: {this.state.time}</p>
+                            <p><span className="event-info">Location</span>: {this.state.location}</p>
+                            <hr />
+                            <p className="list-group-item-text">
+                                {this.state.description}
+                            </p>
+                            <br />
+                            <p>Event created <span className="event-info">{moment(this.state.postTime).fromNow()}</span> by <span className="event-info">{this.state.displayName}</span></p>
+                        </div>
+                        <div>
+                            <Link to={"/events"}>Back to all events</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
